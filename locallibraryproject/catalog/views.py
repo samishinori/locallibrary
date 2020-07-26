8b1f33d1ba21a06fc0d3db271d51cacaeb463cae
test <test@test.com>
2020-07-26 20:07:14 +0900

    OK

---
+++ locallibraryproject/catalog/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+
+# Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre
+import datetime
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+
+    num_genres= Genre.objects.count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+
+    topic_word='fairy'
+    num_topic_word = Book.objects.filter(summary__icontains=topic_word).count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres':num_genres,
+        'topic_word':topic_word,
+        'num_topic_word':num_topic_word,
+        'now':datetime.datetime.now(),
+        'num_visits': num_visits,        
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    paginate_by = 10
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    paginate_by = 10
