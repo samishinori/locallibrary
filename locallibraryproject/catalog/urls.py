f3a2dc4e0fa5844a9410b659b5c8f108a499a5a4
test <test@test.com>
2020-07-27 23:06:06 +0900

    finished before Chap.9 Challenge

--- locallibraryproject/catalog/urls.py
+++ locallibraryproject/catalog/urls.py
@@ -15,3 +15,11 @@ urlpatterns += [
 urlpatterns += [
     path( 'borrowed/', views.AllBorrowedView.as_view(),name='all-borrowed')
     ]
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    ]
+urlpatterns += [  
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+]
